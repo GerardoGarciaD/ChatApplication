a20db8809f01cfbafdf4f8612758c6e4337b969f
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-14 16:35:14 -0600

    Moving files and creating env variables using dotenv

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
+const config = require('./config/app')
+
 const app = express();
 
-port = 3000;
 
 app.get("/home", (req,res)=> {
-    return res.send("Home Screen")
+    return res.send("Home Screen sdfsdf")
 })
+ 
+port = config.appPort;
 
 app.listen(port,()=> {
     console.log(`Server listening on the port ${port}`);
