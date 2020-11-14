372f653bfe8e18c113eacc22ea328540b7b0788a
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-14 17:03:24 -0600

    formatting the code

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,17 @@
-const express = require('express')
+const express = require("express");
 
-const config = require('./config/app')
+const config = require("./config/app");
 
 const app = express();
 
+app.get("/home", (req, res) => {
+  return res.send("Home Screen sdfsdf");
+});
 
-app.get("/home", (req,res)=> {
-    return res.send("Home Screen sdfsdf")
-})
- 
 port = config.appPort;
 
-app.listen(port,()=> {
-    console.log(`Server listening on the port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server listening on the port ${port}`);
+});
 
 console.log("Hello World");
