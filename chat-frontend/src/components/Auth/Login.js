32a3b3b7132c5437e7cf97205aa445ae9e610693
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-15 01:41:50 -0600

    Añadiendo estructura del componente

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,7 +1,34 @@
 import React from "react";
+import loginImage from "../../assets/images/login.svg";
 
 const Login = () => {
-  return <div>Login Screen</div>;
+  return (
+    <div id="auth-container">
+      <div id="auth-card">
+        <div>
+          <div id="image-section">
+            <img src={loginImage} alt="Login" />
+          </div>
+          <div id="form-section">
+            <h2>Welcomme back</h2>
+
+            <form>
+              <div className="input-field">
+                <input type="Wmail" />
+              </div>
+              <div className="input-field">
+                <input type="Password" />
+              </div>
+
+              <button>Login</button>
+            </form>
+
+            <p>Don't have an account? Register</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
