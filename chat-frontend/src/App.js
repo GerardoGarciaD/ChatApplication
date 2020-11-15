c1273c840240638ba135a395d2c05d2ec37a1e5a
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-15 01:35:14 -0600

    Implementando react-router-dom para el render de los diferentes componentes

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Chat from "./components/Chat/Chat";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Chat App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Chat}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+
+          {/* Esto se renderiza cuando el Switch no encuentra ninguna ruta */}
+          <Route render={() => <h1> 404 Page not found</h1>}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
