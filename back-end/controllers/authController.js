5f6e628807971f923cc1ce5829838418a2b539aa
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-14 23:39:10 -0600

    Implementando JWT

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,6 +1,7 @@
 // Se obtiene el modelo de usuario para hacer busqueda en la BD
 const User = require("../models").User;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -20,12 +21,25 @@ exports.login = async (req, res) => {
     if (!bcrypt.compareSync(password, user.password))
       return res.status(401).json({ message: "Incorrect credentials" });
 
-    return res.send(user);
+    //   Generar auth token, se añade la opcion de user.get y raw, para mandar un formato simplificado de la respuesta que se obtiene con el modelo de sequelize que pueda leer
+    const userWithToken = generateToken(user.get({ raw: true }));
+    return res.send(userWithToken);
   } catch (e) {
-    console.log(e);
+    return res.status(500).json({ message: e.message });
   }
 
   return res.send([email, password]);
 };
 
 exports.register = async (req, res) => {};
+
+const generateToken = (user) => {
+  console.log(user);
+
+  // se quita el campo password de la respuesta
+  delete user.password;
+
+  const token = jwt.sign(user, "secret", { expiresIn: 86400 });
+
+  return { ...user, token };
+};
