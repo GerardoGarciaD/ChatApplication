f61fde0082b12a6fdfa01ca5e806a9f7daa6a253
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-16 01:45:26 -0600

    Revert "Añadiendo seguridad a la ruta de chat"
    This reverts commit 228edcbc1501db8115d152a8c1a4e4b206d9e800.

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,16 +3,16 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Chat from "./components/Chat/Chat";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ProtectedRoute from "./components/Router/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <ProtectedRoute exact path="/" component={Chat}></ProtectedRoute>
+          <Route exact path="/" component={Chat}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/register" component={Register}></Route>
+
           {/* Esto se renderiza cuando el Switch no encuentra ninguna ruta */}
           <Route render={() => <h1> 404 Page not found</h1>}></Route>
         </Switch>
