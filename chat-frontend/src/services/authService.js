710c1c634eab14c8626bd732fdc02d83e251d5f9
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-16 00:27:30 -0600

    Añadiendo ruta al servicio para registrar usuario

--- chat-frontend/src/services/authService.js
+++ chat-frontend/src/services/authService.js
@@ -16,6 +16,22 @@ const AuthService = {
         })
     );
   },
+
+  register: (data) => {
+    return (
+      API.post("/register", data)
+        // Se hace destructuring para obtener solo el objeto data de la respuesta al llamado
+        .then(({ data }) => {
+          //   Se añade el header de autorization al llamado
+          API.defaults.headers["Authorization"] = `Bearer ${data.token}`;
+          return data;
+        })
+        .catch((err) => {
+          console.log("Auth service error", err);
+          throw err;
+        })
+    );
+  },
 };
 
 export default AuthService;
