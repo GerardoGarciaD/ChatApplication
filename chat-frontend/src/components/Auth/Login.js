0187eeae2675e8df31556ec3e9109bd68df8f156
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-15 22:10:12 -0600

    Hooks para campos email y passwod, y llamado al endpoint con axios

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImage from "../../assets/images/login.svg";
 import "./Auth.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://127.0.0.1:3000/login", { email, password })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(`err ${err}`);
+      });
+    console.log({ email, password });
+  };
   return (
     <div id="auth-container">
       <div id="auth-card">
@@ -14,12 +31,24 @@ const Login = () => {
           <div id="form-section">
             <h2>Welcome back</h2>
 
-            <form>
+            <form onSubmit={submitForm}>
               <div className="input-field mb-1">
-                <input type="email" placeholder="Email" />
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  required="required"
+                  value={email}
+                  placeholder="Email"
+                />
               </div>
               <div className="input-field mb-2">
-                <input type="password" placeholder="Password" />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  required="required"
+                  value={password}
+                  placeholder="Password"
+                />
               </div>
 
               <button>Login</button>
