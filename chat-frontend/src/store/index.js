5adc78b720b20d66d21e0f74dd0f07c76facb214
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-16 15:34:07 -0600

    Instalando extención para reducer-dev-tools

--- chat-frontend/src/store/index.js
+++ chat-frontend/src/store/index.js
@@ -4,6 +4,10 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import { composeWithDevTools } from "redux-devtools-extension";
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
