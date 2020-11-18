58bf6324a34b2fb6e8057f5abd2e826cdfb188bc
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-17 21:55:40 -0600

    Middleware de autorización para verificar y obtener información del token

---
+++ back-end/middleware/auth.js
@@ -0,0 +1,26 @@
+const { request } = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("../config/app");
+
+exports.auth = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  //   el doble && se utiliza para indicar que,si existe authHeader entonces que se haga el split en donde se elimina la primer parte del token "Bearer (token)"
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Missing token!" });
+  }
+
+  //   Se verifica el token con el id guardado en env
+  jwt.verify(token, config.appKey, (errr, user) => {
+    if (err) {
+      return res.status(401).json({ err });
+    }
+
+    // Aqui es donde se añade la informacion obtenida del usuario al REQUEST, para que asi se pueda saber que usuario es el que esta haciendo la peticio
+    req.user = user;
+  });
+
+  next();
+};
