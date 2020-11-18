94e0b30dee36a134e8922c740f27864c0d43f0cf
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-17 22:38:59 -0600

    Añadiendo reglas de validación para la ruta

--- back-end/router/user.js
+++ back-end/router/user.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { update } = require("../controllers/userController");
 const { auth } = require("../middleware/auth");
+const { rules: updateRules } = require("../validators/auth/register");
 const { validate } = require("../validators");
 
-router.post("/update", [auth], update);
+// Primero se manda a llamar al middleware de auth para verificar el token, despues se añaden la reglas que se desean verificar y por ultimo se valida si es que existe algún error al momemento que se hicieron las validaciones
+router.post("/update", [auth, updateRules, validate], update);
 
 module.exports = router;
