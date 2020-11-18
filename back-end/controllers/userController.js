c5462eeabd0fccd4176461e566a34067a7bc086c
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-17 22:44:37 -0600

    Lógica para hacer update del usuario

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,6 +1,28 @@
 const User = require("../models").User;
-const sequerlize = require("sequelize");
+const sequelize = require("sequelize");
 
-exports.update = (req, res) => {
-  return res.send("User Controller");
+exports.update = async (req, res) => {
+  try {
+    //   Se manda a llamar el metodo que Update que se proporciona desde el modelo
+    const [rows, result] = await User.update(req.body, {
+      where: {
+        //   req.user es el que se obtiene cuando pasa por el middleware de atuh
+        id: req.user.id,
+      },
+      //   Esto es para que regrese la lista de los elementos que modificó
+      returning: true,
+      //   Y esto es para que se puedan ejecutar las funciones individuales de cada atributo del modelo User
+      individualHooks: true,
+    });
+
+    // Se obtiene el primer resultado y se convierte en formato plano de texto
+    const user = result[0].get({ raw: true });
+    // Y por ultimo se manda a llamar la funcion avatar del modelo para poder obtener el avatar del usuario
+    user.avatar = result[0].avatar;
+    delete user.password;
+
+    return res.send(user);
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
 };
