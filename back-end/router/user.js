4b6efaf8c95bd6ef9280842a8c2fa331fe2e51f6
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-18 22:09:30 -0600

    Se añade el middleware para poder manejar las imagenes

--- back-end/router/user.js
+++ back-end/router/user.js
@@ -3,8 +3,9 @@ const { update } = require("../controllers/userController");
 const { auth } = require("../middleware/auth");
 const { rules: updateRules } = require("../validators/auth/register");
 const { validate } = require("../validators");
+const { userFile } = require("../middleware/fileUpload");
 
-// Primero se manda a llamar al middleware de auth para verificar el token, despues se añaden la reglas que se desean verificar y por ultimo se valida si es que existe algún error al momemento que se hicieron las validaciones
-router.post("/update", [auth, updateRules, validate], update);
+// Primero se manda a llamar al middleware de userFile para poder manejar las imagenes de los avatar, despues auth para verificar el token, despues se añaden la reglas que se desean verificar y por ultimo se valida si es que existe algún error al momemento que se hicieron las validaciones
+router.post("/update", [auth, userFile, updateRules, validate], update);
 
 module.exports = router;
