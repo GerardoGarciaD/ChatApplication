a61515839201990b3881f98b5d08dee738f7171b
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-19 22:23:29 -0600

    Creación de interseptor para validar token

--- chat-frontend/src/services/api.js
+++ chat-frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import store from "../store";
+import { logout } from "../store/actions/auth";
 
 // Se crea una nueva instancia de axios para evitar escribir el baseURL en todos los llamados
-export default axios.create({
+const API = axios.create({
   baseURL: "http://127.0.0.1:3000",
   headers: {
     Accept: "application/json",
@@ -9,3 +11,25 @@ export default axios.create({
     Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
   },
 });
+
+// Se utiliza este interceptor para verificar si el token aun es valido
+API.interceptors.response.use(
+  // Si no hay error, entonces se regresa la respuesta
+  (res) => {
+    return res;
+  },
+
+  (err) => {
+    if (err.response.status !== 401) {
+      throw err;
+    }
+
+    // err.name es el mensaje que ya esta definido por JWT para cuando el token esta expirado
+    if (typeof err.response.data.err.name !== "undefined") {
+      store.dispatch(logout());
+      throw err;
+    }
+  }
+);
+
+export default API;
