e2175c2cd718425aa2bcba22389d42d21be5d69c
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-21 02:27:19 -0600

    Implementando action para obtener los chats del usuario

--- chat-frontend/src/components/Chat/Chat.js
+++ chat-frontend/src/components/Chat/Chat.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
 import "./Chat.scss";
+import { fetchChats } from "../../store/actions/chat";
 
 const Chat = () => {
+  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.authReducer.user);
+
+  useEffect(() => {
+    dispatch(fetchChats())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, [dispatch]);
+
+  const chats = useSelector((state) => state.chatReducer.chats);
   return (
     <div id="chat-container">
       <Navbar></Navbar>
