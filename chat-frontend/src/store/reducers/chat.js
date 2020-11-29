1e55e874cf51bfaeef3295c10ad193bf6b424ff1
Gerardo García Dueñez <geraz26@hotmail.com>
2020-11-29 02:15:12 -0600

    Implementación del caso FRIENDS_ONLINE para cambiar el status de todos los usuarios conectados a online

--- chat-frontend/src/store/reducers/chat.js
+++ chat-frontend/src/store/reducers/chat.js
@@ -1,4 +1,10 @@
-const { FETCH_CHATS, SET_CURRENT_CHAT } = require("../actions/chat");
+const {
+  FETCH_CHATS,
+  SET_CURRENT_CHAT,
+  FRIENDS_ONLINE,
+  FRIEND_ONLINE,
+  FRIEND_OFFLINE,
+} = require("../actions/chat");
 
 const initalState = {
   chats: [],
@@ -19,6 +25,27 @@ const chatReducer = (state = initalState, action) => {
       return {
         ...state,
         currentChat: payload,
+      };
+
+    case FRIENDS_ONLINE:
+      const chatsCopy = state.chats.map((chat) => {
+        return {
+          ...chat,
+          Users: chat.Users.map((user) => {
+            if (payload.includes(user.id)) {
+              return {
+                ...user,
+                status: "online",
+              };
+            }
+            return user;
+          }),
+        };
+      });
+
+      return {
+        ...state,
+        chats: chatsCopy,
       };
 
     default:
