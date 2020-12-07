009ce5d7e37cecc1118fa1fbc1d20aa2a2d3b188
Gerardo García Dueñez <geraz26@hotmail.com>
2020-12-07 00:56:22 -0600

    implementando codigo para enviar emojis

--- chat-frontend/src/components/Chat/components/MessageInput/MessageInput.js
+++ chat-frontend/src/components/Chat/components/MessageInput/MessageInput.js
@@ -1,15 +1,19 @@
 import React, { useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./MessageInput.scss";
 import ChatService from "../../../../services/chatService";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+import "./MessageInput.scss";
 
 const MessageInput = ({ chat }) => {
   const user = useSelector((state) => state.authReducer.user);
   const socket = useSelector((state) => state.chatReducer.socket);
   const fileUpload = useRef();
+  const msgInput = useRef();
   const [message, setMessage] = useState("");
   const [image, setImage] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   const handleMessage = (e) => {
     const value = e.target.value;
@@ -57,6 +61,7 @@ const MessageInput = ({ chat }) => {
 
     setMessage("");
     setImage("");
+    setShowEmojiPicker(false);
 
     // TODO: Send message with socket
     socket.emit("message", msg);
@@ -74,6 +79,21 @@ const MessageInput = ({ chat }) => {
       })
       .catch((err) => console.log(err));
   };
+
+  const selectEmoji = (emoji) => {
+    const startPosition = msgInput.current.selectionStart;
+    const endPosition = msgInput.current.selectionEnd;
+    const emojiLength = emoji.native.length;
+    const value = msgInput.current.value;
+    setMessage(
+      value.substring(0, startPosition) +
+        emoji.native +
+        value.substring(endPosition, value.length)
+    );
+    msgInput.current.focus();
+    msgInput.current.selectionEnd = endPosition + emojiLength;
+  };
+
   return (
     <div id="input-container">
       <div id="image-upload-container">
@@ -107,6 +127,7 @@ const MessageInput = ({ chat }) => {
 
       <div id="message-input">
         <input
+          ref={msgInput}
           value={message}
           type="text"
           placeholder="Message..."
@@ -115,6 +136,7 @@ const MessageInput = ({ chat }) => {
         />
 
         <FontAwesomeIcon
+          onClick={() => setShowEmojiPicker(!showEmojiPicker)}
           icon={["far", "smile"]}
           className="fa-icon"
         ></FontAwesomeIcon>
@@ -126,6 +148,15 @@ const MessageInput = ({ chat }) => {
         type="file"
         onChange={(e) => setImage(e.target.files[0])}
       ></input>
+
+      {showEmojiPicker ? (
+        <Picker
+          title="Pick your emoji"
+          emoji="point_up"
+          style={{ position: "absolute", bottom: "20px", right: "20px" }}
+          onSelect={selectEmoji}
+        ></Picker>
+      ) : null}
     </div>
   );
 };
