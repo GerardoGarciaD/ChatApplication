f0bcac043f6c72b86eb8cc356b3d9d036db629d0
Gerardo García Dueñez <geraz26@hotmail.com>
2020-12-12 17:09:43 -0600

    router para crear endpoint para buscar a los usuarios

--- back-end/router/user.js
+++ back-end/router/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { update } = require("../controllers/userController");
+const { update, search } = require("../controllers/userController");
 const { auth } = require("../middleware/auth");
 const { rules: updateRules } = require("../validators/user/user");
 const { validate } = require("../validators");
@@ -7,5 +7,6 @@ const { userFile } = require("../middleware/fileUpload");
 
 // Primero se manda a llamar al middleware de userFile para poder manejar las imagenes de los avatar, despues auth para verificar el token, despues se añaden la reglas que se desean verificar y por ultimo se valida si es que existe algún error al momemento que se hicieron las validaciones
 router.post("/update", [auth, userFile, updateRules, validate], update);
+router.get("/search-users", [auth], search);
 
 module.exports = router;
