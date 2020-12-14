760d2ace4ca141bbe49aaad73b667de24aae7623
Gerardo García Dueñez <geraz26@hotmail.com>
2020-12-13 18:47:23 -0600

    Routers para añadir usuario al grupo y dejar chat

--- back-end/router/chat.js
+++ back-end/router/chat.js
@@ -5,6 +5,8 @@ const {
   messages,
   deleteChat,
   imageUpload,
+  addUserToGroup,
+  leaveCurrentChat,
 } = require("../controllers/chatController");
 const { auth } = require("../middleware/auth");
 const { chatFile } = require("../middleware/fileUpload");
@@ -13,6 +15,9 @@ router.get("/", [auth], index);
 router.get("/messages", [auth], messages);
 router.post("/create", [auth], create);
 router.post("/upload-image", [auth, chatFile], imageUpload);
+router.post("/add-user-to-group", [auth], addUserToGroup);
+router.post("/leave-current-chat", [auth], leaveCurrentChat);
+
 router.delete("/:id", [auth], deleteChat);
 
 module.exports = router;
